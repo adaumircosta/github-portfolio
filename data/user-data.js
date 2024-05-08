a4ef3a2181a7a89b15c6a777b430e04fca76a0a3
Adaumir Costa <adaumircosta@gmail.com>
2024-05-08 04:11:51 -0400

    project

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'adaumircosta',
+  devUsername: "adaumircosta",
+  github: 'https://github.com/adaumircosta',
+  facebook: 'https://www.facebook.com/adaumirc/',
+  linkedIn: 'https://www.linkedin.com/in/adaumir-costa/',
+  twitter: 'https://twitter.com/brfellas',
+  stackOverflow: 'https://stackoverflow.com/users/2886042/adaumir',
+  leetcode: "https://leetcode.com/adaumircosta/",
+  resume: "https://drive.google.com/file/d/1TYpKN91CVwvAykKWiz_5Cp2PT8HlfI2e/view?usp=sharing",
+  skills: ['Java', 'Spring', 'Quarkus', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'Azure'],
+  timezone: '-4'
+};
